48336345ab89f3d496a96436ab6064bcce353acc
Ian <ViViDboarder@gmail.com>
2012-07-02 13:25:36 -0400

    Switched to vundle

--- vim-sync-append.sh
+++ vim-sync-append.sh
@@ -3,6 +3,15 @@
 # Get current directory for future use in links
 VIM_SYNC_DIR=${PWD}
 
+# Clone vundle
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# Make backup and tmp dirs
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/tmp
+mkdir -p ~/.vim/doc
+
 # If a .vimrc_sync doesn't exist, link it
 if [[ ! ( -f ~/.vimrc_sync ) ]]; then
 	ln -s $VIM_SYNC_DIR/vim/dot_vimrc ~/.vimrc_sync
@@ -31,8 +40,11 @@ if ! ( grep -q 'set runtimepath+=$HOME/.vim_sync' ~/.vimrc ); then
 	echo 'set runtimepath+=$HOME/.vim_sync' >> ~/.vimrc
 fi
 
+# Install all bundles
+vim +BundleInstall! +qall
+
 # Execute vim's update of the helptags
-vim +"helptags ~/.vim_sync/doc" +"q"
+vim +"helptags ~/.vim/doc" +"q"
 
 echo "Should install ctags with sudo apt-get install ctags"
 
