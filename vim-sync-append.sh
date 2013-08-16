5bfb7cc24d21f7560bd8179170fbe2a6f58a1702
ViViDboarder <ViViDboarder@gmail.com>
2013-08-16 14:30:54 -0700

    Clean whitespaces

--- vim-sync-append.sh
+++ vim-sync-append.sh
@@ -15,8 +15,8 @@ cd $VIM_SYNC_DIR
 VIM_SYNC_DIR=$(pwd)
 
 # Verify git is installed (although needed to checkout
-command -v git >/dev/null 2>&1 || { 
-    echo "Error: git required for install"; 
+command -v git >/dev/null 2>&1 || {
+    echo "Error: git required for install";
     exit 1;
 }
 
@@ -36,34 +36,34 @@ mkdir -p ~/.vim/doc
 # If a .vimrc_sync doesn't exist, link it
 if [[ ! ( -f ~/.vimrc_sync ) ]]; then
     echo "Linking sync'd vimrc"
-	ln -s $VIM_SYNC_DIR/vim/dot_vimrc ~/.vimrc_sync
+    ln -s $VIM_SYNC_DIR/vim/dot_vimrc ~/.vimrc_sync
 fi
 
 # If a .vim_drpobox dir doesn't exist, link it
 if [[ ! ( -d ~/.vim_sync ) ]]; then
     echo "Linking sync'd vim runtime"
-	ln -s $VIM_SYNC_DIR/vim/dot_vim ~/.vim_sync
+    ln -s $VIM_SYNC_DIR/vim/dot_vim ~/.vim_sync
 fi
 
 # if there is no .vimrc file already, make a blank one
 if [[ ( ! -f ~/.vimrc ) ]]; then
     echo "No ~/.vimrc found, creating one"
-	touch ~/.vimrc
+    touch ~/.vimrc
 fi
 
 # if not already sourcing the synced version, source it
 if ! ( grep -q 'source ~/.vimrc_sync' ~/.vimrc ); then
     echo "Source sync'd vimrc in vimrc"
-	echo '' >> ~/.vimrc
-	echo '"import vimrc from synced' >> ~/.vimrc
-	echo 'source ~/.vimrc_sync' >> ~/.vimrc
+    echo '' >> ~/.vimrc
+    echo '"import vimrc from synced' >> ~/.vimrc
+    echo 'source ~/.vimrc_sync' >> ~/.vimrc
 fi
 
 if ! ( grep -q 'set runtimepath+=$HOME/.vim_sync' ~/.vimrc ); then
     echo "Add sync'd vim dir to runtime"
-	echo '' >> ~/.vimrc
-	echo '"add vim directory from synced' >> ~/.vimrc
-	echo 'set runtimepath+=$HOME/.vim_sync' >> ~/.vimrc
+    echo '' >> ~/.vimrc
+    echo '"add vim directory from synced' >> ~/.vimrc
+    echo 'set runtimepath+=$HOME/.vim_sync' >> ~/.vimrc
 fi
 
 # Install all bundles
@@ -93,11 +93,11 @@ command -v gcc >/dev/null 2>&1 || { echo "Warning: gcc required for Command T"; 
 VIM_RESULT=$(vim +"helptags ~/.vim/doc" +"q")
 
 if [[ "$VIM_RESULT" == *SEGV* ]]; then
-	echo "Seg Faulted. Retry with different ruby"
-	cd ~/.vim/bundle/Command-T/ruby/command-t && /opt/local/bin/ruby* extconf.rb && make && cd -
-	
-	# Retry
-	vim +"helptags ~/.vim/doc" +"q"
+    echo "Seg Faulted. Retry with different ruby"
+    cd ~/.vim/bundle/Command-T/ruby/command-t && /opt/local/bin/ruby* extconf.rb && make && cd -
+
+    # Retry
+    vim +"helptags ~/.vim/doc" +"q"
 fi
 
 # Warn if ctags does not exist
