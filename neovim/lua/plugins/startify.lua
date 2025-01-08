ab094dc95268e7336bbdab23380c5a7fdf5efc84
ViViDboarder <ViViDboarder@gmail.com>
2025-01-07 17:04:27 -0800

    Add default startify bookmarks

--- neovim/lua/plugins/startify.lua
+++ neovim/lua/plugins/startify.lua
@@ -8,3 +8,11 @@ vim.g.startify_list_order = {
     { "   My Sessions" },
     "sessions",
 }
+
+vim.g.startify_bookmarks = {
+    "~/Documents/Obsidian",
+    "~/workspace/vim-settings",
+    "~/workspace/shoestrap",
+    "~/.config/fish",
+    "~/Nextcloud/Notes",
+}
