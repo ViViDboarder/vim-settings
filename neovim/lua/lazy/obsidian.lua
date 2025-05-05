a5c3a10516c201ceb21978cbb0cd8153c399c17d
Vividboarder <vividboarder@gmail.com>
2025-05-05 09:05:11 -0700

    Move obsidian config to one file

--- neovim/lua/lazy/obsidian.lua
+++ neovim/lua/lazy/obsidian.lua
@@ -5,6 +5,123 @@
 -- and commits to my vault git repo. On iOS devices, I use Working Copy to sync the
 -- repo and use Shortcuts to automate pulling on open and auto committing and pushing
 -- after closing Obsidian.
+
+local vault_path = vim.fn.expand("~/Documents/Obsidian")
+
+local function auto_git()
+    -- Set up auto pull and commit
+    local group_id = vim.api.nvim_create_augroup("obsidian-git", { clear = true })
+
+    -- Create auto pull on open
+    local autopull = function()
+        local Job = require("plenary.job")
+        vim.notify("Pulling Obsidian notes", vim.log.levels.DEBUG, { title = "Obsidian" })
+        ---@diagnostic disable-next-line: missing-fields
+        Job:new({
+            command = "git",
+            args = { "pull" },
+            on_exit = function(j, return_val)
+                if return_val == 0 then
+                    vim.notify("Pulled Obsidian notes", vim.log.levels.INFO, { title = "Obsidian" })
+                else
+                    vim.notify(
+                        "Failed to pull Obsidian notes. " .. vim.inspect(j:result()),
+                        vim.log.levels.ERROR,
+                        { title = "Obsidian" }
+                    )
+                end
+            end,
+        }):start()
+    end
+
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = vault_path .. "/**",
+        callback = autopull,
+        group = group_id,
+    })
+
+    local Job = require("plenary.job")
+
+    -- Create autocommit on save
+    local auto_add = function(next_func)
+        return function(ev)
+            ---@diagnostic disable-next-line: missing-fields
+            Job:new({
+                command = "git",
+                args = { "add", ev.file },
+                on_exit = function(add_j, add_return_val)
+                    if add_return_val ~= 0 then
+                        vim.notify(
+                            "Failed to add file to git. " .. vim.inspect(add_j:result()),
+                            vim.log.levels.ERROR,
+                            { title = "Obsidian" }
+                        )
+                        return
+                    end
+
+                    if next_func then
+                        next_func()
+                    end
+                end,
+            }):start()
+        end
+    end
+
+    local auto_commit = function(next_func)
+        return function()
+            local date_string = os.date("%Y-%m-%d %H:%M:%S")
+            ---@diagnostic disable-next-line: missing-fields
+            Job:new({
+                command = "git",
+                args = { "commit", "-m", "Auto commit: " .. date_string },
+                on_exit = function(commit_j, commit_return_val)
+                    if commit_return_val ~= 0 then
+                        vim.notify(
+                            "Failed to commit file to git. " .. vim.inspect(commit_j:result()),
+                            vim.log.levels.ERROR,
+                            { title = "Obsidian" }
+                        )
+                        return
+                    end
+                    if next_func then
+                        next_func()
+                    end
+                end,
+            }):start()
+        end
+    end
+
+    local auto_push = function(next_func)
+        return function()
+            ---@diagnostic disable-next-line: missing-fields
+            Job:new({
+                command = "git",
+                args = { "push" },
+                on_exit = function(push_j, push_return_val)
+                    if push_return_val ~= 0 then
+                        vim.notify(
+                            "Failed to push Obsidian notes. " .. vim.inspect(push_j:result()),
+                            vim.log.levels.ERROR,
+                            { title = "Obsidian" }
+                        )
+                    end
+
+                    if next_func then
+                        next_func()
+                    end
+                end,
+            }):start()
+        end
+    end
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        pattern = vault_path .. "/**",
+        callback = auto_add(auto_commit(auto_push())),
+        group = group_id,
+    })
+end
+
+-- Return plugin spec
 return {
     "https://github.com/obsidian-nvim/obsidian.nvim",
     dependencies = {
@@ -13,7 +130,7 @@ return {
     version = "^3",
     opts = {
         workspaces = {
-            { name = "personal", path = require("plugins.obsidian").vault_path },
+            { name = "personal", path = vault_path },
         },
         ui = {
             checkboxes = {
@@ -50,11 +167,13 @@ return {
         },
     },
     config = function(_, opts)
-        require("plugins.obsidian").config(opts)
+        require("obsidian").setup(opts)
+
+        auto_git()
     end,
     event = {
-        "BufRead " .. require("plugins.obsidian").vault_path .. "/**",
-        "BufNewFile " .. require("plugins.obsidian").vault_path .. "/**",
+        "BufRead " .. vault_path .. "/**",
+        "BufNewFile " .. vault_path .. "/**",
     },
     cmd = {
         "ObsidianOpen",
