55abde9cf03184e1e35559ee3b8d0487850aa7b6
ViViDboarder <ViViDboarder@gmail.com>
2025-07-08 14:26:13 -0700

    Remove start and end to scope blanklines

--- neovim/lua/lazy_plugins.lua
+++ neovim/lua/lazy_plugins.lua
@@ -67,6 +67,11 @@ return {
         ---@module "ibl"
         ---@type ibl.config
         opts = {
+            scope = {
+                enabled = true,
+                show_start = false,
+                show_end = false,
+            },
             exclude = {
                 filetypes = {
                     "help",
