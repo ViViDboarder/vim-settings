1d16e4be8719095011cf8e025a6e2322785e8631
Vividboarder <vividboarder@gmail.com>
2026-04-10 16:45:33 -0700

    Don't use ui2 in neovide due to bug

--- neovim/lua/config/init.lua
+++ neovim/lua/config/init.lua
@@ -4,12 +4,13 @@ local utils = require("utils")
 utils.require_with_local("config.variables")
 utils.require_with_local("config.settings")
 utils.require_with_local("config.bindings")
-if vim.fn.has("nvim-0.12.0") == 1 then
-    utils.require_with_local("config.ui")
-end
 
 if vim.g.neovide then
     require("config.neovide")
+elseif vim.fn.has("nvim-0.12.0") == 1 then
+    -- UI2 is broken with in neovide
+    -- https://github.com/neovide/neovide/issues/3446
+    utils.require_with_local("config.ui")
 end
 
 -- Plugins
