f392f181db7f46b349d3bc18828c2f57e35d563f
ViViDboarder <ViViDboarder@gmail.com>
2026-04-14 20:51:03 -0700

    Fixup ts

--- neovim/lua/config/pack.lua
+++ neovim/lua/config/pack.lua
@@ -279,28 +279,7 @@ packle.add({
             "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
         },
         after = function()
-            -- NOTE: This could possibly move into ftplugin files
-            local enable_fts = utils.require_with_local("config.plugins.treesitter").ensure_installed
-            local ts_gid = vim.api.nvim_create_augroup("treesitter_fts", { clear = true })
-            vim.opt.foldlevel = 99
-            vim.opt.foldlevelstart = 2
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = enable_fts,
-                callback = function()
-                    local nvim_ts = require("nvim-treesitter")
-                    local installed_parsers = nvim_ts.get_installed()
-                    if not vim.list_contains(installed_parsers, vim.bo.filetype) then
-                        nvim_ts.install(vim.bo.filetype):wait(10000)
-                    end
-
-                    -- Enable highlighting
-                    vim.treesitter.start()
-                    -- Enable folding
-                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                    vim.wo[0][0].foldmethod = "expr"
-                end,
-                group = ts_gid,
-            })
+            utils.require_with_local("config.plugins.treesitter").setup()
         end,
     },
 })
